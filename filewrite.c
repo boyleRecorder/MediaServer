6748eda5d0327cca4f523e32e65cab858b3ebeee
Paul Boyle@ <paulcboyle@gmail.com>
2010-06-30 19:33:54 +0100

    Removed the include of malloc. Now compiles on osx

---
+++ filewrite.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include "filewrite.h"
+
+
+
+static void* fileWritingThread(void *arg)
+{
+}
+
+
+
+
+// 
+// Public functions.
+//
+FileWriteHandle* getNewHandle(char *fileName)
+{
+}
+
+void initialiseFileWriting()
+{
+}
+
+void closeFileWriteHandle(FileWriteHandle* handle)
+{
+}
+
+void writeDataChunk(FileWriteHandle *handle,struct bufferData *data)
+{
+}
+
+
