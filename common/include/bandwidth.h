c604263434f9324fb94a38f9e480da056e21b793
Paul Boyle <paulcboyle@gmail.com>
2010-07-16 19:24:43 +0100

    moved codec enum to separate file.

---
+++ common/include/bandwidth.h
@@ -0,0 +1,17 @@
+#ifndef BANDWIDTH_H__
+#define BANDWIDTH_H__
+
+/**
+  Bandwidth enumerator.
+  Lists the supported bandwidths. 
+  */
+enum BandWidth{
+   /// 8Khz audio
+  BANDWIDTH_8K,
+  /// 16 Khz
+  BANDWIDTH_16K
+};
+
+
+
+#endif
